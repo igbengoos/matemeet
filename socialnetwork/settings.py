1b063d697fbb7572d1df85b622eb016021e5b74b
Oluwagbenga Adeleye <71673671+igbengoos@users.noreply.github.com>
2024-04-06 06:53:46 +0100

    Storage Settings Added settings.py

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -159,6 +159,9 @@ MEDIA_URL = '/media/'
 
 STORAGES = {
     # ...
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
