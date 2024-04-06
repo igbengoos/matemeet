102a2e752d2558398672f61bc537b71af095bd5a
Oluwagbenga Adeleye <71673671+igbengoos@users.noreply.github.com>
2024-04-06 07:07:09 +0100

    Just remove storage in the settings.py

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -150,22 +150,9 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_URL = 'static/'
-
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
 
-
-STORAGES = {
-    # ...
-    "default": {
-        "BACKEND": "django.core.files.storage.FileSystemStorage",
-    },
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
