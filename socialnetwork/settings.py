9eeeb81633f36989b913242590753e38e0995c54
Oluwagbenga Adeleye <71673671+igbengoos@users.noreply.github.com>
2024-04-06 06:36:32 +0100

    Added database to settings.py

--- socialnetwork/settings.py
+++ socialnetwork/settings.py
@@ -101,8 +101,12 @@ WSGI_APPLICATION = 'socialnetwork.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'koyebdb',
+        'USER': 'koyeb-adm',
+        'PASSWORD': 'oKv24fYPcdjt',
+        'HOST': 'ep-bold-salad-a2a23r4v.eu-central-1.pg.koyeb.app',
+        'OPTIONS': {'sslmode': 'require'},
     }
 }
 
